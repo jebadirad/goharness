f8a40a56cddcab9358dd9513f7994e678c9e3b16
jebadirad <jondavid.ebadirad@asu.edu>
2023-08-13 01:52:57 -0700

    remove preload

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,6 @@ import { LoginClient } from "./_login/LoginClient";
 import { themes } from "@/themes/index";
 import { getTheme } from "../_getTheme";
 
-export const preload = () => {
-  void getTheme();
-};
-
 export default async function Login() {
   const allThemes = await getTheme();
   return <LoginClient themes={allThemes || themes} />;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import { HomeClient } from "./_home/HomeClient";
 import { getTheme } from "./_getTheme";
 import { themes } from "@/themes";
 
-export const preload = () => {
-  void getTheme();
-};
-
 export default async function Home() {
   await getTheme();
 
